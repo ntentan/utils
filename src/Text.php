4ac99a4a3a4afd1dc1aed153c4dc977625ced2d6
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-04-20 08:06:16 +0000

    Working on the testing and trying to release a first stable version

--- src/Text.php
+++ src/Text.php
@@ -1,8 +1,46 @@
 <?php
+/*
+ * Ntentan Framework
+ * Copyright (c) 2008-2015 James Ekow Abaka Ainooson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ * 
+ */
+
 namespace ntentan\utils;
 
+/**
+ * A couple of utility functions for manipulating strings.
+ */
 class Text
 {
+    /**
+     * Converts text separated by a specified separator to camel case. This 
+     * function converts the entire text into lower case before performing the
+     * camel case conversion. Due to this the first character would be
+     * lower cased.
+     * 
+     * @param string $string The text to be converted.
+     * @param string $separator The separator to consider for camel casing
+     * @return string
+     */
     public static function camelize($string, $separator = '_')
     {
         if(is_array($separator))
@@ -23,11 +61,27 @@ class Text
         );
     }
     
+    /**
+     * Converts text separated by a specified separator to camel case. This 
+     * method works just as the Text::camelize method except that it converts
+     * the first character to uppercase.
+     * 
+     * @param string $string The text to be converted.
+     * @param string $separator The separator to consider for camel casing
+     * @return string
+     */
     public static function ucamelize($string, $separator = '_')
     {
         return ucfirst(self::camelize($string, $separator));
     }
     
+    /**
+     * Converts camel case text into text separated with an arbitrary separator.
+     * 
+     * @param string $string The text to be converted.
+     * @param string $separator The separator to be used.
+     * @return string
+     */    
     public static function deCamelize($string, $separator = '_')
     {
         return preg_replace_callback(
