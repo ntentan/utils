f386c10cf43040d5818f940574f20310da62e969
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-07-28 08:20:29 +0000

    Some documentation here and there

--- src/Validator.php
+++ src/Validator.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Ntentan Framework
  * Copyright (c) 2008-2015 James Ekow Abaka Ainooson
@@ -26,27 +27,60 @@
 
 namespace ntentan\utils;
 
+/**
+ * Base validator class for validating data found in arrays.
+ */
 class Validator {
 
+    /**
+     * An array which represnts the validation rules
+     * @var array
+     */
     private $rules = [];
+    
+    /**
+     * An array which holds the validation errors found after the last
+     * validation was run.
+     * @var array
+     */
     private $invalidFields = [];
+    
+    /**
+     * A temporal storage for validation messages
+     * @var string
+     */
     private $message;
 
+    /**
+     * Returns a new instance of the Validator
+     * @return \ntentan\utils\Validator
+     */
     public static function getInstance() {
         return new Validator();
     }
 
+    /**
+     * Set the validation rules
+     * @param array $rules
+     */
     public function setRules($rules) {
         $this->rules = $rules;
     }
 
-    private function callValidation($ruleIndex, $rule, &$messages, $data)
-    {
-        if(!is_numeric($ruleIndex)) {
+    /**
+     * Call the method for a given validation.
+     * 
+     * @param string|integer $ruleIndex
+     * @param string $rule
+     * @param array $messages
+     * @param mixed $data
+     * @return boolean
+     */
+    private function callValidation($ruleIndex, $rule, &$messages, $data) {
+        if (!is_numeric($ruleIndex)) {
             $method = $ruleIndex;
             $options = $rule;
-        }
-        else {
+        } else {
             $method = $rule;
             $options = null;
         }
@@ -54,18 +88,20 @@ class Validator {
         $method = "validate$method";
 
         $response = $this->$method($data['data'], $data['field'], $options);
-        if($response) {
+        if ($response) {
             return true;
-        }
-        else {
+        } else {
             $messages[] = $this->message;
             return false;
         }
-
     }
 
-    public function getInvalidFields()
-    {
+    /**
+     * Returns an associative array of all the errors that occured after the
+     * last validation was run.
+     * @return array
+     */
+    public function getInvalidFields() {
         return $this->invalidFields;
     }
 
@@ -77,15 +113,14 @@ class Validator {
     public function validate($data) {
         $passed = true;
         $this->invalidFields = [];
-        foreach($this->rules as $field => $fieldRules) {
+        foreach ($this->rules as $field => $fieldRules) {
             $fieldMessages = [];
-            foreach($fieldRules as $ruleIndex => $rule) {
+            foreach ($fieldRules as $ruleIndex => $rule) {
                 $this->callValidation(
-                    $ruleIndex, $rule, $fieldMessages,
-                    ['field' => $field, 'data' => $data[$field]]
+                        $ruleIndex, $rule, $fieldMessages, ['field' => $field, 'data' => $data[$field]]
                 );
             }
-            if(count($fieldMessages) > 0) {
+            if (count($fieldMessages) > 0) {
                 $this->invalidFields[$field] = $fieldMessages;
                 $passed = false;
             }
@@ -94,10 +129,9 @@ class Validator {
     }
 
     protected function evaluateResult($result, $message, $options) {
-        if($result) {
+        if ($result) {
             return true;
-        }
-        else {
+        } else {
             $this->message = isset($options['message']) ? $options['message'] : $message;
             return false;
         }
@@ -105,19 +139,29 @@ class Validator {
 
     protected function validateRequired($data, $name, $options) {
         return $this->evaluateResult(
-            $data !== null && $data !== '',
-            "The {$name} field is required",
-            $options
+            $data !== null && $data !== '', 
+            "The {$name} field is required", $options
         );
     }
 
     protected function validateRegexp($data, $name, $options) {
-        if(preg_match_all($options, $data)) {
-            return true;
-        }
-        else {
-            return "The {$name} field has an invalid format";
-        }
+        return $this->evaluateResult(
+            preg_match_all(is_string($options) ? $options : $options[0], $data), 
+            "The {$name} field is required", $options
+        );
+    }
+
+    protected function validateNumeric($data, $name, $options) {
+        return $this->evaluateResult(
+            is_numeric($data), "The {$name} field is not numeric", $options
+        );
+    }
+    
+    protected function validateLength($data, $name, $options) {
+        return $this->evaluateResult(
+            strlen($data) > $options,
+            "The {$name} field cannot have its lenght greater than {$options}"
+        );
     }
 
 }
