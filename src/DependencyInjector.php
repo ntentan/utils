54fc40b67994071366955eb9c09ff85c3c6de8d4
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-11-12 15:54:52 +0000

    Working dependency injector

--- src/DependencyInjector.php
+++ src/DependencyInjector.php
@@ -4,14 +4,28 @@ namespace ntentan\utils;
 
 trait DependencyInjector
 {
-    private $injectedDependencies = [];
+    protected $loadedDependencies = [];
     
-    public function __call($method, $args)
+    private static $resolver;
+    
+    public function getDependency($dependency)
     {
-        foreach($this->injectedDependencies as $depnendency) {
-            if(method_exists($depnendency, $args)) {
-                return call_user_func_array([$depnendency, $method], $args);
-            }
+        if(isset($this->loadedDependencies[$dependency])) {
+            return $this->loadedDependencies[$dependency];
         }
+    }
+    
+    protected function loadDependency($dependency, $params = null)
+    {
+        $resolver = self::$resolver;
+        $className = $resolver($dependency);
+        $dependencyInstance = new $className($params);
+        $this->loadedDependencies[Text::camelize($dependency)] = $dependencyInstance;
+        return $dependencyInstance;
+    }
+    
+    public static function setDependencyResolver($resolver)
+    {
+        self::$resolver = $resolver;
     }
 }
