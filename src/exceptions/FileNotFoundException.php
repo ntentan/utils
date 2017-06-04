1cc8c6a52ec207199d4c72159cae372a85cd0c06
James Ainooson <jainooson@gmail.com>
2017-06-03 21:33:56 -0500

    Added a message to file not found exception Hope this doesn't break anything

--- src/exceptions/FileNotFoundException.php
+++ src/exceptions/FileNotFoundException.php
@@ -4,5 +4,7 @@ namespace ntentan\utils\exceptions;
 
 class FileNotFoundException extends FilesystemException
 {
-    
+    public function __construct($path) {
+        parent::__construct("File '$path' not found");
+    }
 }
