9d8248e043b7ed0914637355c7f795c4f4f3b769
James Ainooson <jainooson@gmail.com>
2017-07-24 18:34:34 -0500

    Adding whitelisting for utils testing

--- tests/cases/ValidatorTest.php
+++ tests/cases/ValidatorTest.php
@@ -1,11 +1,13 @@
 <?php
+
 namespace ntentan\utils\tests\cases;
 
 use ntentan\utils\Validator;
 use PHPUnit\Framework\TestCase;
 
-class ValidatorTest extends TestCase 
+class ValidatorTest extends TestCase
 {
+
     private $validator;
 
     public function setUp()
@@ -13,138 +15,142 @@ class ValidatorTest extends TestCase
         $this->validator = Validator::getInstance();
     }
 
-    public function testRequired() {
+    public function testRequired()
+    {
         $this->validator->setRules(
-            array(
-                'required' => ['name']
-            )
+                array(
+                    'required' => ['name']
+                )
         );
         $response = $this->validator->validate(
-            array(
-                'name' => null
-            )
-        );
-        $this->assertEquals(false, $response);
-        $this->assertEquals(['name' => ['The name field is required']], $this->validator->getInvalidFields());
-
-        $response = $this->validator->validate(
-            array(
-                'name' => ''
-            )
+                array(
+                    'name' => null
+                )
         );
         $this->assertEquals(false, $response);
         $this->assertEquals(['name' => ['The name field is required']], $this->validator->getInvalidFields());
 
         $response = $this->validator->validate(
-            array(
-                'name' => 'Jamie!'
-            )
+                array(
+                    'name' => ''
+                )
+        );
+        $this->assertEquals(false, $response);
+        $this->assertEquals(['name' => ['The name field is required']], $this->validator->getInvalidFields());
+
+        $response = $this->validator->validate(
+                array(
+                    'name' => 'Jamie!'
+                )
         );
         $this->assertEquals(true, $response);
         $this->assertEquals([], $this->validator->getInvalidFields());
-        
+
         $response = $this->validator->validate([]);
         $this->assertEquals(false, $response);
         $this->assertEquals(['name' => ['The name field is required']], $this->validator->getInvalidFields());
     }
 
-    public function testRequiredMessage() {
+    public function testRequiredMessage()
+    {
         $this->validator->setRules(
-            ['required' => ['name' => ['message' => 'Please provide a name']]]
+                ['required' => ['name' => ['message' => 'Please provide a name']]]
         );
         $response = $this->validator->validate(
-            array(
-                'name' => ''
-            )
+                array(
+                    'name' => ''
+                )
         );
         $this->assertEquals(false, $response);
         $this->assertEquals(['name' => ['Please provide a name']], $this->validator->getInvalidFields());
     }
-    
-    public function testRegexpValidation() {
+
+    public function testRegexpValidation()
+    {
         $this->validator->setRules(
-            ['regexp' => ['name' => '/[A-Z]+/']]
+                ['regexp' => ['name' => '/[A-Z]+/']]
         );
-        
+
         $response = $this->validator->validate(
-            ['name' => 'james']
+                ['name' => 'james']
         );
         $this->assertEquals(false, $response);
         $this->assertEquals(
-            ['name' => ['The format of your input is invalid']], 
-            $this->validator->getInvalidFields()
+                ['name' => ['The format of your input is invalid']], $this->validator->getInvalidFields()
         );
-        
+
         $response = $this->validator->validate(
-            ['name' => 'JAMES']
+                ['name' => 'JAMES']
         );
         $this->assertEquals(true, $response);
     }
-    
-    public function testNumericValidation() {
+
+    public function testNumericValidation()
+    {
         $this->validator->setRules(
-            ['numeric' => ['age']]
+                ['numeric' => ['age']]
         );
         $response = $this->validator->validate(
-            ['age' => '21a']
+                ['age' => '21a']
         );
         $this->assertEquals(false, $response);
         $this->assertEquals(
-            ['age' => ['The age field must contain only numbers']], 
-            $this->validator->getInvalidFields()
+                ['age' => ['The age field must contain only numbers']], $this->validator->getInvalidFields()
         );
-        
+
         $response = $this->validator->validate(['age' => '21']);
         $this->assertEquals(true, $response);
     }
-    
-    public function testLengthValidation() {
+
+    public function testLengthValidation()
+    {
         $this->validator->setRules(
-            ['length' => ['user_name' => 8]]
+                ['length' => ['user_name' => 8]]
         );
         $response = $this->validator->validate(
-            ['user_name' => 'james.ainooson']
+                ['user_name' => 'james.ainooson']
         );
         $this->assertEquals(false, $response);
         $this->assertEquals(
-            ['user_name' => ['The length of the user_name field must be 8 characters or less']],
-            $this->validator->getInvalidFields()
+                ['user_name' => ['The length of the user_name field must be 8 characters or less']], $this->validator->getInvalidFields()
         );
-        
+
         $response = $this->validator->validate(
-            ['user_name' => 'james']
+                ['user_name' => 'james']
         );
         $this->assertEquals(true, $response);
     }
-     
-    public function testLenghtMinMaxValidation() {
+
+    public function testLenghtMinMaxValidation()
+    {
         $this->validator->setRules(
-            ['length' => ['user_name' => ['min' => 6, 'max' => 8]]]
+                ['length' => ['user_name' => ['min' => 6, 'max' => 8]]]
         );
         $response = $this->validator->validate(
-            ['user_name' => 'james.ainooson']
+                ['user_name' => 'james.ainooson']
         );
         $this->assertEquals(false, $response);
         $this->assertEquals(
-            ['user_name' => ['The length of the user_name field must be 6 characters or greater and 8 characters or lesser']],
-            $this->validator->getInvalidFields()
-        );    
+                ['user_name' => ['The length of the user_name field must be 6 characters or greater and 8 characters or lesser']], $this->validator->getInvalidFields()
+        );
     }
-    
+
     /**
      * @expectedException \ntentan\utils\exceptions\ValidatorException
      */
-    public function testValidatorException() {
+    public function testValidatorException()
+    {
         $this->validator->setRules(
-            ['fake_validator' => ['name']]
+                ['fake_validator' => ['name']]
         );
-        
+
         $this->validator->validate(
-            ['name' => 'not important']
+                ['name' => 'not important']
         );
     }
-    
-    public function testCustomValidation() {
+
+    public function testCustomValidation()
+    {
         $customValidation = new \ntentan\utils\tests\lib\FakeValidator();
         $customValidation->setRules(
             ['fake' => [[['username', 'name']]]]
@@ -159,10 +165,11 @@ class ValidatorTest extends TestCase
         $this->assertEquals(false, $response);
         $this->assertEquals(
             [
-                'name' => ['Username must be lowercase form of name'], 
+                'name' => ['Username must be lowercase form of name'],
                 'username' => ['Username must be lowercase form of name']
-            ],
+            ], 
             $customValidation->getInvalidFields()
         );
     }
+
 }
